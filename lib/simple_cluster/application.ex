5a2f2faf137101dd76e594677d757946e9d020bc
ryan <ryanyogan@gmail.com>
2021-06-30 12:17:33 -0500

    basic static usage of libcluster

--- lib/simple_cluster/application.ex
+++ lib/simple_cluster/application.ex
@@ -6,11 +6,26 @@ defmodule SimpleCluster.Application do
   @impl true
   def start(_type, _args) do
     children = [
+      {Cluster.Supervisor, [topologies(), [name: SimpleCluster.ClusterSupervisor]]},
       SimpleCluster.Observer,
       SimpleCluster.Ping
     ]
 
     opts = [strategy: :one_for_one, name: SimpleCluster.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp topologies do
+    [
+      example: [
+        strategy: Cluster.Strategy.Epmd,
+        config: [
+          hosts: [
+            :"n1@127.0.0.1",
+            :"n2@127.0.0.1"
+          ]
+        ]
+      ]
+    ]
   end
 end
